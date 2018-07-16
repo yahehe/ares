4560fcf8f58c29db1c7fecf37cbf7a5d9742c5b7
Ciaran Downey <yahehe4837@gmail.com>
2018-07-16 01:04:50 -0400

    Base dependencies

---
+++ src/app.js
@@ -0,0 +1,32 @@
+
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.json({ strict: false }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// Get User endpoint
+app.get('/users/:userId', function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  }
+})
+
+// Create User endpoint
+app.post('/users', function (req, res) {
+  const { userId, name } = req.body;
+  if (typeof userId !== 'string') {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== 'string') {
+    res.status(400).json({ error: '"name" must be a string' });
+  }
+})
+
+module.exports = app;
